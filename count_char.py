3ae890e88d3f1e618bb7e8909656c41dc297cfe5
vantdo222 <dothuyvanhb@gmail.com>
2024-09-22 17:36:52 -0600

    Add files via upload

---
+++ count_char.py
@@ -0,0 +1,2 @@
+def count_char(word):
+    print(word+' has '+str(len(word))+' characters.')
---
+++ saying_hello.py
@@ -0,0 +1,3 @@
+def saying_hello():
+    name = input("What's your name? ")
+    print('Hello '+name.capitalize()+', nice to meet you!')
